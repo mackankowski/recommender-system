bca9bcb06aa2b734ae79307f98c834b691083481
Maciej Kankowski <mackankowski@gmail.com>
2018-12-15 22:42:09 +0100

    Add rest endpoint with user-based recommender

--- src/main/java/recommender/Application.java
+++ src/main/java/recommender/Application.java
@@ -1,7 +1,5 @@
 package recommender;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -11,11 +9,6 @@ public class Application {
     
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
     }
 
 }
--- src/main/java/recommender/UserBasedController.java
+++ src/main/java/recommender/UserBasedController.java
@@ -12,23 +12,29 @@ import org.apache.mahout.cf.taste.neighborhood.UserNeighborhood;
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.apache.mahout.cf.taste.recommender.UserBasedRecommender;
 import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 //source -> https://mahout.apache.org/users/recommender/userbased-5-minutes.html
-
-public class UserBased {
-    public void create(){
+@RestController
+public class UserBasedController {
+    @RequestMapping("/user-based")
+    public String print(){
         try {
-            DataModel model = new FileDataModel(new File("/path/to/dataset.csv"));
+            ClassLoader classLoader = getClass().getClassLoader();
+            File file = new File(classLoader.getResource("datasets.csv").getFile());
+            DataModel model = new FileDataModel(new File(file.getAbsolutePath()));
             UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
             UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
             UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
             List<RecommendedItem> recommendations = recommender.recommend(2, 2);
             for (RecommendedItem recommendation : recommendations) {
-                System.out.println(recommendation);
+                return recommendation.toString();
             }
         } catch (Exception e)
         {
-            System.out.println("Exception occured !");
+            return e.toString();
         }
+        return "";
     }
 }
